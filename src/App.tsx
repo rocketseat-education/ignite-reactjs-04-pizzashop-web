53827b1e8ad087de2ced896efc25fa5527cd7d31
Vini Fraga <vinifragam@gmail.com>
2023-12-14 11:46:25 -0300

    Setup do projeto - Configurando ESLint e Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Button } from './components/ui/button'
 import './global.css'
+
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className="flex h-screen items-center justify-center">
       <Button>Enviar</Button>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "node:path"
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
