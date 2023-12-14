dff627bb985a7d0ca6b4bc60fa36e274306ef459
Vini Fraga <vinifragam@gmail.com>
2023-12-14 11:36:52 -0300

    Setup do Projeto - Instalando shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
+    </div>
   )
 }
