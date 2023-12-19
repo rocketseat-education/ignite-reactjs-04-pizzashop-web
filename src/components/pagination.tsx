9010a172f8b27e2831693c55f04a5dba9fa7ca3b
Vini Fraga <vinifragam@gmail.com>
2023-12-19 16:50:39 -0300

    Páginas e componentes (UI) - Componente: Paginação

---
+++ src/components/pagination.tsx
@@ -0,0 +1,54 @@
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from 'lucide-react'
+
+import { Button } from './ui/button'
+
+interface PaginationProps {
+  pageIndex: number
+  totalCount: number
+  perPage: number
+}
+
+export function Pagination({
+  pageIndex,
+  perPage,
+  totalCount,
+}: PaginationProps) {
+  const pages = Math.ceil(totalCount / perPage) || 1
+
+  return (
+    <div className="flex items-center justify-between">
+      <span className="text-sm text-muted-foreground">
+        Total de {totalCount} item(s)
+      </span>
+
+      <div className="flex items-center gap-6 lg:gap-8">
+        <div className="text-sm font-medium">
+          Página {pageIndex + 1} de {pages}
+        </div>
+        <div className="flex items-center gap-2">
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronsLeft className="h-4 w-4" />
+            <span className="sr-only">Primeira página</span>
+          </Button>
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Página anterior</span>
+          </Button>
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronRight className="h-4 w-4" />
+            <span className="sr-only">Próxima página</span>
+          </Button>
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronsRight className="h-4 w-4" />
+            <span className="sr-only">Última página</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Pagination } from '@/components/pagination'
 import {
   Table,
   TableBody,
@@ -18,30 +19,32 @@ export function Orders() {
 
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFilters />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
-                <TableHead className="w-[140px]">Identificador</TableHead>
-                <TableHead className="w-[180px]">Realizado há</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-[140px]">Total do pedido</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <OrderTableRow key={i} />
-              })}
-            </TableBody>
-          </Table>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
