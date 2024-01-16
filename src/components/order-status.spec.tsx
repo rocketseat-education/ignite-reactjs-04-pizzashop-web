6d56c1f735c372eedd5f994c43aac655dac9d124
Vini Fraga <vinifragam@gmail.com>
2024-01-16 16:12:38 -0300

    Testes e mocks - Teste unitário: Order Status

--- src/components/order-status.spec.tsx
+++ src/components/order-status.spec.tsx
@@ -1,3 +1,55 @@
-test('1 + 1 equals 2', () => {
-  expect(1 + 1).toEqual(2)
+import { render } from '@testing-library/react'
+
+import { OrderStatus } from './order-status'
+
+describe('Order Status', () => {
+  it('should display the right text when order status is pending', () => {
+    const wrapper = render(<OrderStatus status="pending" />)
+
+    const statusText = wrapper.getByText('Pendente')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-slate-400')
+  })
+
+  it('should display the right text when order status is canceled', () => {
+    const wrapper = render(<OrderStatus status="canceled" />)
+
+    const statusText = wrapper.getByText('Cancelado')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-rose-500')
+  })
+
+  it('should display the right text when order status is delivering', () => {
+    const wrapper = render(<OrderStatus status="delivering" />)
+
+    const statusText = wrapper.getByText('Em entrega')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-amber-500')
+  })
+
+  it('should display the right text when order status is processing', () => {
+    const wrapper = render(<OrderStatus status="processing" />)
+
+    const statusText = wrapper.getByText('Em preparo')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-amber-500')
+  })
+
+  it('should display the right text when order status is delivered', () => {
+    const wrapper = render(<OrderStatus status="delivered" />)
+
+    const statusText = wrapper.getByText('Entregue')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-emerald-500')
+  })
 })
--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -21,19 +21,31 @@ export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === 'pending' && (
-        <span className="h-2 w-2 rounded-full bg-slate-400" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-slate-400"
+        />
       )}
 
       {status === 'canceled' && (
-        <span className="h-2 w-2 rounded-full bg-rose-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-rose-500"
+        />
       )}
 
       {status === 'delivered' && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-emerald-500"
+        />
       )}
 
       {['processing', 'delivering'].includes(status) && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-amber-500"
+        />
       )}
 
       <span className="font-medium text-muted-foreground">
