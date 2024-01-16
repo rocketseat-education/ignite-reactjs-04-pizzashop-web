b80acda54062461d099551a5a1a468c8c8e22417
Vini Fraga <vinifragam@gmail.com>
2024-01-16 18:09:36 -0300

    Testes e mocks - Teste unitário: Sign In

---
+++ src/pages/auth/sign-in.spec.tsx
@@ -0,0 +1,32 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import { HelmetProvider } from 'react-helmet-async'
+import { MemoryRouter } from 'react-router-dom'
+
+import { queryClient } from '@/lib/react-query'
+
+import { SignIn } from './sign-in'
+
+describe('SignIn', () => {
+  it('should set default email input value if email is present on search params', () => {
+    const wrapper = render(<SignIn />, {
+      wrapper: ({ children }) => {
+        return (
+          <HelmetProvider>
+            <MemoryRouter
+              initialEntries={['/sign-in?email=johndoe@example.com']}
+            >
+              <QueryClientProvider client={queryClient}>
+                {children}
+              </QueryClientProvider>
+            </MemoryRouter>
+          </HelmetProvider>
+        )
+      },
+    })
+
+    const emailInput = wrapper.getByLabelText('Seu e-mail') as HTMLInputElement
+
+    expect(emailInput.value).toEqual('johndoe@example.com')
+  })
+})
