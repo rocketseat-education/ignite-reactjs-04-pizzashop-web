a7f2ea8264a2798c601910a1bc464114df98c78d
Vini Fraga <vinifragam@gmail.com>
2024-01-17 18:29:03 -0300

    Testes e mocks - Mock de autenticação

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,7 +2,9 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
-export const worker = setupWorker()
+import { signInMock } from './sign-in-mock'
+
+export const worker = setupWorker(signInMock)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
---
+++ src/api/mocks/sign-in-mock.ts
@@ -0,0 +1,21 @@
+import { http, HttpResponse } from 'msw'
+
+import { SignInBody } from '../sign-in'
+
+export const signInMock = http.post<never, SignInBody>(
+  '/authenticate',
+  async ({ request }) => {
+    const { email } = await request.json()
+
+    if (email === 'johndoe@example.com') {
+      return new HttpResponse(null, {
+        status: 200,
+        headers: {
+          'Set-Cookie': 'auth=sample-jwt',
+        },
+      })
+    }
+
+    return new HttpResponse(null, { status: 401 })
+  },
+)
