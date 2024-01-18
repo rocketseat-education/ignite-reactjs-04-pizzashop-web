ef889bb806fe12d9f08f1da4b49c5ad64f9fcbf8
Vini Fraga <vinifragam@gmail.com>
2024-01-18 18:24:13 -0300

    Testes e mocks - Teste E2E: Métricas do Dashboard

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test'
+
+test('display day orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('20', { exact: true })).toBeVisible()
+  expect(page.getByText('-5% em relação a ontem')).toBeVisible()
+})
+
+test('display month orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('200', { exact: true })).toBeVisible()
+  expect(page.getByText('+7% em relação ao mês passado')).toBeVisible()
+})
+
+test('display month canceled orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('5', { exact: true })).toBeVisible()
+  expect(page.getByText('-5% em relação ao mês passado')).toBeVisible()
+})
+
+test('display month revenue metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  expect(page.getByText('R$ 200,00')).toBeVisible()
+  expect(page.getByText('+10% em relação ao mês passado')).toBeVisible()
+})
