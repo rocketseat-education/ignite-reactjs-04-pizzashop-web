cbc8002a7abb0bcd60e0a6058769008e28b90a77
Vini Fraga <vinifragam@gmail.com>
2024-01-18 18:50:47 -0300

    Testes e mocks - Teste E2E: Pedidos

---
+++ test/orders.e2e-spec.ts
@@ -0,0 +1,85 @@
+import { expect, test } from '@playwright/test'
+
+test('list orders', async ({ page }) => {
+  await page.goto('/orders', { waitUntil: 'networkidle' })
+
+  expect(
+    page.getByRole('cell', { name: 'Customer 1', exact: true }),
+  ).toBeVisible()
+  expect(
+    page.getByRole('cell', { name: 'Customer 10', exact: true }),
+  ).toBeVisible()
+})
+
+test('paginate orders', async ({ page }) => {
+  await page.goto('/orders', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Próxima página' }).click()
+
+  expect(
+    page.getByRole('cell', { name: 'Customer 11', exact: true }),
+  ).toBeVisible()
+  expect(
+    page.getByRole('cell', { name: 'Customer 20', exact: true }),
+  ).toBeVisible()
+
+  await page.getByRole('button', { name: 'Última página' }).click()
+
+  expect(
+    page.getByRole('cell', { name: 'Customer 51', exact: true }),
+  ).toBeVisible()
+  expect(
+    page.getByRole('cell', { name: 'Customer 60', exact: true }),
+  ).toBeVisible()
+
+  await page.getByRole('button', { name: 'Página anterior' }).click()
+
+  expect(
+    page.getByRole('cell', { name: 'Customer 41', exact: true }),
+  ).toBeVisible()
+  expect(
+    page.getByRole('cell', { name: 'Customer 50', exact: true }),
+  ).toBeVisible()
+
+  await page.getByRole('button', { name: 'Primeira página' }).click()
+
+  expect(
+    page.getByRole('cell', { name: 'Customer 1', exact: true }),
+  ).toBeVisible()
+  expect(
+    page.getByRole('cell', { name: 'Customer 10', exact: true }),
+  ).toBeVisible()
+})
+
+test('filter by order id', async ({ page }) => {
+  await page.goto('/orders', { waitUntil: 'networkidle' })
+
+  await page.getByPlaceholder('ID do pedido').fill('order-11')
+  await page.getByRole('button', { name: 'Filtrar resultados' }).click()
+
+  expect(page.getByRole('cell', { name: 'order-11' })).toBeVisible()
+})
+
+test('filter by customer name', async ({ page }) => {
+  await page.goto('/orders', { waitUntil: 'networkidle' })
+
+  await page.getByPlaceholder('Nome do cliente').fill('Customer 11')
+  await page.getByRole('button', { name: 'Filtrar resultados' }).click()
+
+  expect(page.getByRole('cell', { name: 'Customer 11' })).toBeVisible()
+})
+
+test('filter by status', async ({ page }) => {
+  await page.goto('/orders', { waitUntil: 'networkidle' })
+
+  await page.getByRole('combobox').click()
+  await page.getByLabel('Pendente').click()
+
+  await page.getByRole('button', { name: 'Filtrar resultados' }).click()
+
+  const tableRows = await page.getByRole('cell', { name: 'Pendente' }).all()
+
+  expect(tableRows).toHaveLength(10)
+
+  await page.waitForTimeout(500)
+})
