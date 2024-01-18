e1ced879f6a05a5d34bd0065ce83efdcb0ccd922
Vini Fraga <vinifragam@gmail.com>
2024-01-18 18:23:53 -0300

    Testes e mocks - Teste E2E: Edição de perfil

---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test'
+
+test('update profile successfully', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill('Rocket Pizza')
+  await page.getByLabel('Descrição').fill('Another Description')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Perfil atualizado com sucesso!')
+
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+
+  await page.waitForTimeout(250)
+
+  expect(page.getByRole('button', { name: 'Rocket Pizza' })).toBeVisible()
+})
